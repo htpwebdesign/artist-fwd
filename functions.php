30e128e1dc8cb4de70145b1d0e6cf42171acdf71
Peter Nguyen <peternguyenwebdev@gmail.com>
2023-03-22 22:48:07 -0700

    functions.php API update

--- functions.php
+++ functions.php
@@ -199,13 +199,13 @@ if ( class_exists( 'WooCommerce' ) ) {
 
  // Method 1: Filter.
 function my_acf_google_map_api( $api ){
-    $api['key'] = 'AIzaSyD1k2HJ2MXMKWyjPD4_NeXqTmBLPWgKRZU';
+    $api['key'] = 'AIzaSyATzLvecNFYJ9ZoVr-bW3MNUxS7Cfd9vw4';
     return $api;
 }
 add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
 
 // Method 2: Setting.
 // function my_acf_init() {
-//     acf_update_setting('google_api_key', 'AIzaSyD1k2HJ2MXMKWyjPD4_NeXqTmBLPWgKRZU');
+//     acf_update_setting('google_api_key', 'AIzaSyATzLvecNFYJ9ZoVr-bW3MNUxS7Cfd9vw4');
 // }
 // add_action('acf/init', 'my_acf_init');
