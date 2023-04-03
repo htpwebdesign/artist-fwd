4fce5d930b6738a22eedcab9289b592665f5dff2
Peter Nguyen <peternguyenwebdev@gmail.com>
2023-04-03 14:28:56 -0700

    Mobile update page about

--- page-about.php
+++ page-about.php
@@ -22,31 +22,33 @@ get_header();
 			the_post();
 			?>
 			<div class="entry-content">
-
-			<?php
-				if (function_exists( 'get_field_object' ) ) :
-					
-					if ( get_field_object( 'about') ) :
-						$field = get_field_object('about');
-						?>
-						<section class="about-section">
-							<h1 class="page-title"><?php echo $field['label']; ?></h1>
-							<p><?php echo esc_html($field['value']); ?></p>
-						</section>
-						<?php 
-					endif;
-
-				endif;
-
-				if ( function_exists( 'get_field' ) ) :
-					?>
-					<section class="about-accessories"></section>
-						<?php
+				<section class="about-section">
+					<?php
+					if ( function_exists( 'get_field' ) ) :
 						if ( get_field( 'portrait') ) :
 							$portrait = get_field( 'portrait' );
 							echo wp_get_attachment_image( $portrait, 'medium' );
 						endif;
-						
+					endif;
+
+					if (function_exists( 'get_field_object' ) ) :
+						if ( get_field_object( 'about') ) :
+							$field = get_field_object('about');
+							?>
+							
+								<h1 class="page-title"><?php echo $field['label']; ?></h1>
+								<p><?php echo esc_html($field['value']); ?></p>
+							<?php 
+						endif;
+					endif;
+				?>
+				</section>
+				<?php
+
+				if ( function_exists( 'get_field' ) ) :
+					?>
+					<section class="about-accessories">
+						<?php
 						if ( get_field( 'cv_link') ) :
 							?>
 							<a class="faux-btn" href="<?php the_field('cv_link'); ?>" target="_blank" rel="noopener noreferrer">CV Button</a> 
@@ -69,15 +71,23 @@ get_header();
 						$collabObj = get_field_object('collab_links');
 						$collabs = get_field( 'collab_links' );
 						?>
-						<h2><?php echo esc_html($collabObj['label']); ?></h2>
-						<?php
-
-						foreach( $collabs as $collab ) :
-							?> 
-							<a href="<?php echo esc_url($collab['collab_name']['url']); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($collab['collab_name']['title']); ?></a>
+						<section class="about-collabs">
+							<h2><?php echo esc_html($collabObj['label']); ?></h2>
 							<?php
-						endforeach;
 
+							$len = count($collabs);
+							foreach( $collabs as $collab ) :
+								?> 
+								<a class="collaborator" href="<?php echo esc_url($collab['collab_name']['url']); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($collab['collab_name']['title']); ?></a>
+								<?php
+								if( $len > 1) :
+									echo '<br>'; 
+									$len--;
+								endif;
+							endforeach;
+							?>
+						</section>
+						<?php
 					endif;
 
 				endif;
