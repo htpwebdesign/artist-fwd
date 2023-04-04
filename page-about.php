c0abd9c963496c88f5e9053dea7f284995b0782e
elisa-he <he.elisa1998@gmail.com>
2023-04-03 17:31:14 -0700

    Merge branch 'main' of https://github.com/htpwebdesign/artist-fwd

--- page-about.php
+++ page-about.php
@@ -15,19 +15,19 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
+    <?php
 		while ( have_posts() ) :
 			the_post();
 			?>
-			<div class="entry-content">
-				<section class="about-section">
-					<?php
+    <div class="entry-content">
+        <section class="about-section">
+            <?php
 					if ( function_exists( 'get_field' ) ) :
 						if ( get_field( 'portrait') ) :
 							$portrait = get_field( 'portrait' );
-							echo wp_get_attachment_image( $portrait, 'medium' );
+							echo wp_get_attachment_image( $portrait, 'large' );
 						endif;
 					endif;
 
@@ -35,69 +35,73 @@ get_header();
 						if ( get_field_object( 'about') ) :
 							$field = get_field_object('about');
 							?>
-							
-								<h1 class="page-title"><?php echo $field['label']; ?></h1>
-								<p><?php echo esc_html($field['value']); ?></p>
-							<?php 
+
+            <h1 class="page-title"><?php echo $field['label']; ?></h1>
+            <p><?php echo esc_html($field['value']); ?></p>
+            <?php 
 						endif;
 					endif;
 				?>
-				</section>
-				<?php
-
-				if ( function_exists( 'get_field' ) ) :
-					?>
-					<section class="about-accessories">
-						<?php
-						if ( get_field( 'cv_link') ) :
-							?>
-							<a class="faux-btn" href="<?php the_field('cv_link'); ?>" target="_blank" rel="noopener noreferrer">CV Button</a> 
-							<?php
-						endif;
-
-						if ( get_field( 'portfolio_cta') ) :
-							?>
-							<a class="faux-btn" href="<?php the_field('portfolio_cta'); ?>" target="_blank" rel="noopener noreferrer">CV Button</a> 
-							<?php
-						endif;
-					?>
-					</section>
-					<?php
-				endif;
-
+        </section>
+        <?php
 				if ( function_exists( 'get_field' ) ) :
 
 					if ( get_field( 'collab_links') ) :
 						$collabObj = get_field_object('collab_links');
 						$collabs = get_field( 'collab_links' );
 						?>
-						<section class="about-collabs">
-							<h2><?php echo esc_html($collabObj['label']); ?></h2>
-							<?php
+        <section class="about-collabs">
+            <h2><?php echo esc_html($collabObj['label']); ?></h2>
+            <?php
 
 							$len = count($collabs);
 							foreach( $collabs as $collab ) :
-								?> 
-								<a class="collaborator" href="<?php echo esc_url($collab['collab_name']['url']); ?>" target="_blank" rel="noopener noreferrer"><?php echo esc_html($collab['collab_name']['title']); ?></a>
-								<?php
+								?>
+            <a class="collaborator" href="<?php echo esc_url($collab['collab_name']['url']); ?>" target="_blank"
+                rel="noopener noreferrer"><?php echo esc_html($collab['collab_name']['title']); ?></a>
+            <?php
 								if( $len > 1) :
 									echo '<br>'; 
 									$len--;
 								endif;
 							endforeach;
 							?>
-						</section>
-						<?php
+        </section>
+        <?php
 					endif;
 
 				endif;
+
+				if ( function_exists( 'get_field' ) ) :
+					?>
+        <section class="about-accessories">
+            <?php
+						if ( get_field( 'cv_link') ) :
+							?>
+            <a class="faux-btn" href="<?php the_field('cv_link'); ?>" target="_blank" rel="noopener noreferrer">CV
+                Button</a>
+            <?php
+						endif;
+
+						if ( get_field( 'portfolio_cta') ) :
+							?>
+            <a class="faux-btn" href="<?php the_field('portfolio_cta'); ?>" target="_blank" rel="noopener noreferrer">CV
+                Button</a>
+            <?php
+						endif;
+					?>
+        </section>
+        <?php
+				endif;
+
+		
 				?>
-			</div>
-			<?php
+    </div>
+    <?php
 		endwhile;
 		?>
 
-	</main>
+</main>
 
 <?php
 get_footer();
